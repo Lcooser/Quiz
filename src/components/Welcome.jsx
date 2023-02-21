d7214924a107114dc3ad1ecb6c3bc80d2db506e4
Luiz Coser <luiz.cooser1@gmail.com>
2023-02-20 22:41:33 -0300

    Doing the state logic

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -6,15 +6,13 @@ import './Welcome.css'
 
 
 const Welcome = () => {
-  const quizState = useContext(QuizContext)
-
-  
-
+  const [quizState, dispatch] = useContext(QuizContext)
 
   return (
-    <div>
+    <div id="welcome">
         <h2>Seja bem-vindo</h2>
         <p>Clique no botão abaxio para começar:</p>
+        <button onClick={() => dispatch({type: "CHANGE_STATE"}) }>Iniciar</button>
         <img src={Quiz} alt="Início do Quiz" />
     </div>
   )
