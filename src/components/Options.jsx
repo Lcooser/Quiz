5d5c42d7ea7a0b2178c2d2f4d633664621c8c432
Luiz Coser <luiz.cooser1@gmail.com>
2023-02-22 23:26:29 -0300

    Button continue, function just to show when an option was selected

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -3,13 +3,13 @@ import { QuizContext } from "../context/quiz"
 
 import "./Options.css"
 
-const Options = ({option}) => {
+const Options = ({option, selectOption, answer}) => {
     const [quizState, dispatch] = useContext(QuizContext)
 
 
 
     return (
-        <div className="option">
+        <div className="option" onClick={() => selectOption()}>
             <p>{option}</p>
         </div>
     )
